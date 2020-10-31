cb625e3fc915c4215c519e6c2cbc0d4b2fc5f6ea
jordynatera <jordynatera@users.noreply.github.com>
2020-10-30 23:01:47 -0400

    Hoy creé el for para mostrar datos. Todo funciona, solo debo ver por qué el struct no está sacando datos. ¿Será que no los almacena, en primer lugar?

--- Program.cs
+++ Program.cs
@@ -16,22 +16,22 @@ namespace PrimeraAppMonografico
     {
        
         static void Main(string[] args)
-        { 
-
+        {
             //Crea una app de cons que pida datos de una persona (nombre y año nac) hasta que el usuario diga que no desea
             //continuar. Por cada persona debe calcular y presentar su edad.
-            string respUser = "y";  
-            do
-            {          
-                Console.WriteLine("      GRUPO 12 {0}" +
+            int i = 0;
+            string a;
+            string respUser = "y"; 
+            Persona[] datosPersona = new Persona[100]; 
+            
+             Console.WriteLine("      GRUPO 12 {0}" +
                     "Monografico #44 {0}" +
                     "Jordy Mario Natera Jimenez {0}" +
                     "Neolfi Catalina Roman Ramirez {0}" +
                     "Maicols Zorrilla Natera {0}", Environment.NewLine);
-                int i = 0;
 
-                Persona[] datosPersona = new Persona[100];    
-                
+            do
+            {          
                 Console.WriteLine("Inserte los datos de la persona " + i + "{0}", Environment.NewLine);
                 Console.WriteLine("Nombre: ");
                 datosPersona[i].nombrePersona = Console.ReadLine();
@@ -40,14 +40,26 @@ namespace PrimeraAppMonografico
                 datosPersona[i].anioNacimiento = Console.ReadLine();
 
                 //Prueba
-                Console.WriteLine("{0}PRUEBA DE SALIDA: {0}{0}PERSONA "+i+"{0}Nombre "+ i + ": "+ datosPersona[i].nombrePersona + "{0}", Environment.NewLine);
-                Console.WriteLine("Fecha de Nacimiento de "+ datosPersona[i].nombrePersona + ": " + datosPersona[i].anioNacimiento+ "{0}", Environment.NewLine);
+                Console.WriteLine("{0}PRUEBA DE SALIDA: {0}{0}PERSONA "+i+"{0}Nombre "+ i + ": "+ datosPersona[i].nombrePersona + "{0}",
+                    Environment.NewLine);
+                Console.WriteLine("Fecha de Nacimiento de "+ datosPersona[i].nombrePersona + ": " + datosPersona[i].anioNacimiento+ "{0}",
+                    Environment.NewLine);
                 
                 Console.WriteLine("Desea continuar? (Y/N){0}", Environment.NewLine);
                 respUser = Console.ReadLine();
-                i = i++;
+                i = i+1;
             }
             while (respUser == "y" || respUser == "Y");
+
+            //MOSTRANDO DATOS 
+            Console.WriteLine("Datos de las personas: {0}", Environment.NewLine);
+            for (int j = 0; j <= i; j++)
+            {
+                Console.WriteLine("Persona " + j + " de " + i + "{0}Nombre: " + datosPersona[i].nombrePersona
+                    + "{0}Año de Nacimiento: " + datosPersona[i].anioNacimiento, Environment.NewLine);
+
+            }
+            a = Console.ReadLine();
         }
         
     }  
